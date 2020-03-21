0dbf100a37dcc00a9f555f53e48a1cce72434779
Brennen Green <brennengreen@outlook.com>
2020-03-20 22:37:47 -0400

    Triangle Rasterizer

--- TinyRenderer/main.cpp
+++ TinyRenderer/main.cpp
@@ -6,12 +6,14 @@
 
 const TGAColor WHITE = TGAColor(255, 255, 255, 255);
 const TGAColor RED = TGAColor(255, 0, 0, 255);
+const TGAColor GREEN = TGAColor(0, 255, 0, 255);
 const int WIDTH = 800;
 const int HEIGHT = 800;
 
 Model *model = NULL;
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color);
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color);
 
 int main(int argc, char** argv) {
 	if (argc == 2) {
@@ -22,19 +24,41 @@ int main(int argc, char** argv) {
 	}
 
 	TGAImage image(HEIGHT, WIDTH, TGAImage::RGB);
+
+//  WIREFRAME
+//	for (int i = 0; i < model->nfaces(); i++) {
+//		std::vector<int> face = model->face(i);
+//		for (int j = 0; j < 3; j++) {
+//			Vec3f v0 = model->vert(face[j]);
+//			Vec3f v1 = model->vert(face[(j + 1) % 3]);
+//			int x0 = (v0.x + 1.)*WIDTH / 2.;
+//			int y0 = (v0.y + 1.)*HEIGHT / 2.;
+//			int x1 = (v1.x + 1.)*WIDTH / 2.;
+//			int y1 = (v1.y + 1.)*HEIGHT / 2.;
+//
+//			Vec2i p0(x0, y0);
+//			Vec2i p1(x1, y1);
+//			line(p0, p1, image, WHITE);
+//
+//		}
+//
+//	}
+	Vec3f light_dir(0, 0, -1);
 	for (int i = 0; i < model->nfaces(); i++) {
 		std::vector<int> face = model->face(i);
+		Vec2i screen_coords[3];
+		Vec3f world_coords[3];
 		for (int j = 0; j < 3; j++) {
-			Vec3f v0 = model->vert(face[j]);
-			Vec3f v1 = model->vert(face[(j + 1) % 3]);
-			int x0 = (v0.x + 1.)*WIDTH / 2.;
-			int y0 = (v0.y + 1.)*HEIGHT / 2.;
-			int x1 = (v1.x + 1.)*WIDTH / 2.;
-			int y1 = (v1.y + 1.)*HEIGHT / 2.;
-			line(x0, y0, x1, y1, image, WHITE);
-
+			Vec3f v = model->vert(face[j]);
+			screen_coords[j] = Vec2i((v.x + 1.)*WIDTH / 2., (v.y + 1.)*HEIGHT / 2.);
+			world_coords[j] = v;
 		}
-
+		Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
+		n.normalize();
+		float intensity = n * light_dir;
+		if (intensity > 0) {
+			triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+		}
 	}
 
 	image.flip_vertically(); // Set the origin at the bottom left corner
@@ -43,36 +67,45 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-	bool steep = false;
-	if (std::abs(x0 - x1) < std::abs(y0 - y1)) { // If line is steep, transpose the image
-		std::swap(x0, y0);
-		std::swap(x1, y1);
-		steep = true;
-	}	
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+	if (t0.y == t1.y && t0.y == t2.y) return;
+	if (t0.y > t1.y) std::swap(t0, t1);
+	if (t0.y > t2.y) std::swap(t0, t2);
+	if (t1.y > t2.y) std::swap(t1, t2);
+	int total_height = t2.y - t0.y;
+	for (int i = 0; i < total_height; i++) {
+		bool second_half = i > t1.y - t0.y || t1.y == t0.y;
+		int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+		float alpha = (float)i / total_height;
+		float beta = (float)(i-(second_half ? t1.y-t0.y : 0)) / segment_height;
+		Vec2i A = t0 + (t2 - t0)*alpha;
+		Vec2i B = second_half ? t1 + (t2 - t1)*beta : t0 + (t1 - t0)*beta;
+		if (A.x > B.x) std::swap(A, B);
+		for (int j = A.x; j <= B.x; j++) {
+			image.set(j, t0.y+i, color);
+		}
+	}
+}
 
-	if (x0 > x1) { // Ensure rendering from left-to-right
-		std::swap(x0, x1);
-		std::swap(y0, y1);
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
+	bool steep = false;
+	if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y)) {
+		std::swap(p0.x, p0.y);
+		std::swap(p1.x, p1.y);
+		steep = true;
+	}
+	if (p0.x > p1.x) {
+		std::swap(p0, p1);
 	}
 
-	int dx = x1 - x0;
-	int dy = y1 - y0;
-	float derror2 = std::abs(dy) * 2;
-	float error2 = 0;
-	int y = y0;
-
-	for (int x = x0; x <= x1; x++) {
-		if (steep) { // Detranspose
+	for (int x = p0.x; x <= p1.x; x++) {
+		float t = (x - p0.x) / (float)(p1.x - p0.x);
+		int y = p0.y*(1. - t) + p1.y*t;
+		if (steep) {
 			image.set(y, x, color);
 		}
 		else {
 			image.set(x, y, color);
-		}
-		error2 += derror2;
-		if (error2 > dx) {
-			y += (y1 > y0 ? 1 : -1);
-			error2 -= dx * 2;
 		}
 	}
 }
